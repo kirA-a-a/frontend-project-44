313af1db410d7297b8ee60d14929172b8bc58caf
kirA-a-a <kiraaaakka@gmail.com>
2022-11-29 13:57:13 +0300

    changes in brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,17 +1,16 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync';
 
-import readlineSync from "readline-sync";
-
-export const brainEven = () => {
-
+const brainEven = () => {
     const name = readlineSync.question('May I have your name? ');
     console.log('Welcome to the Brain Games!');
     console.log('Hello, ' + name + '!');
-
     console.log('Answer "yes" if the number is even, otherwise answer "no". ');
 
-    for (let i = 0; i<3; i++) {
 
+    let counter = 0;
+
+    for (let i = 0; i<3; i+=1) {
     let min = 1;
     let max = 10;
 
@@ -21,17 +20,22 @@ export const brainEven = () => {
 
     const answer = readlineSync.question('You answer: ');
 
-
         if (answer === "yes" && randomNum % 2 === 0 ||
             answer === "no" && randomNum % 2 === 1) {
             console.log("Correct!");
+            counter += 1
         } else {
-            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            console.log(`Let's try again, ${name}`)
+            console.log("'yes' is wrong answer ;(. Correct answer was 'no'.");
+            console.log(`Let's try again, ${name}`);
+            break
+        }
+        if (counter === 3) {
+            console.log(`Congratulations, ${name}!`);
         }
     }
 
-    console.log(`Congratulations, ${name}`)
 }
 
 brainEven()
+
+export default {brainEven}
